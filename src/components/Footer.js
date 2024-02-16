02421146d65fb1f1225c7318c44edfa0ecdd2b66
Luna Joffily <luna.joffily@gmail.com>
2024-02-16 12:01:58 -0500

    Footer file finished

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>My Full Stack Development Portfolio</strong> - a React site by{" "}
+          <a href="https://github.com/Lujoffily" target="_blank" rel="noreferrer">
+            Luna Joffily
+          </a>
+          .
+          <hr />
+          <a
+            href="https://www.linkedin.com/in/luna-joffily-b4793a270/"
+            target="_blank" rel="noreferrer"
+          >
+            LinkedIn
+          </a>{" "}
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
